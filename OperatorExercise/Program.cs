355442dbe06b5bde59fb01d0a868475bba3b8ba5
Kamilah <k.muhammad95@gmail.com>
2022-12-28 14:59:51 -0600

    added exercise 1

--- OperatorExercise/Program.cs
+++ OperatorExercise/Program.cs
@@ -4,13 +4,26 @@
     {
         static void Main(string[] args)
         {
+            //EXERCISE 1
             int a = 17;
             int b = 4;
+
+            int sum = a + b;
+            Console.WriteLine($" Sum: {sum}");
+
+            int diff = a - b;
+            Console.WriteLine($" Difference: {diff}");
+
+            int product = a * b;
+            Console.WriteLine($"Product: {product}");
+
             int quotient = a / b;
+
             int remainder = a % b;
+            Console.WriteLine($" a divided by b is {quotient} remainder {remainder}");
 
-            Console.WriteLine($" {a} / {b} is {quotient} remainder {remainder}");
 
+            //EXERCISE 2
             Console.WriteLine("Please enter the radius of your circle:");
             var userInput = Console.ReadLine();
 
